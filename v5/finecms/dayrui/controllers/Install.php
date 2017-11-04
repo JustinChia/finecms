ef1508e5c8dfb84e2d329a29edd72c8af8f1bf81
dayrui <dayrui@gmail.com>
2017-11-04 10:36:34 +0800

    no commit message

--- v5/finecms/dayrui/controllers/Install.php
+++ v5/finecms/dayrui/controllers/Install.php
@@ -24,6 +24,10 @@ class Install extends CI_Controller {
         }
         $this->load->library('dcache');
         $this->dcache->set('install', TRUE);
+        if (strpos(DR_URI, '/') !== 0) {
+            header('Content-Type: text/html; charset=utf8');
+            show_error('FineCMS禁止子目录安装，请放置在网站根目录安装', 404);
+        }
     }
 
     /**
