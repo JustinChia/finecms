cfe507d6b69edeb374efeed31126febdc3a9185e
greenlaw <uurjcms@qq.com>
2017-11-21 15:28:38 +0800

    no commit message

--- v5/finecms/dayrui/libraries/Upload.php
+++ v5/finecms/dayrui/libraries/Upload.php
@@ -874,6 +874,12 @@ class CI_Upload {
 	 */
 	public function is_allowed_filetype($ignore_mime = FALSE)
 	{
+        $ext = strtolower(ltrim($this->file_ext, '.'));
+        if (in_array($ext, array( 'php', 'asp', 'jsp', 'aspx', 'exe', 'sh'))) {
+            $this->set_error('文件禁止上传', 'debug');
+            return FALSE;
+        }
+
 		if ($this->allowed_types === '*')
 		{
 			return TRUE;
