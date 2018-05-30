71483dd454092b6ad5f84201cf66f52906cf2c02
天睿云软件 <dayrui@gmail.com>
2018-05-30 11:18:12 +0800

    no commit message

--- finecms/dayrui/controllers/admin/Upgrade.php
+++ finecms/dayrui/controllers/admin/Upgrade.php
@@ -16,25 +16,39 @@ class Upgrade extends M_Controller {
 
         $this->template->assign(array(
             'menu' => $this->get_menu_v3(array(
-                fc_lang('升级PHP7CMS') => array('admin/upgrade/php7cms', 'refresh'),
+                fc_lang('程序升级') => array('admin/upgrade/index', 'refresh'),
             )),
         ));
-        $this->template->display('php7cms.html');
+        $this->template->display('upgrande.html');
     }
 
-    /**
-     * 程序管理
-     */
-    public function php7cms() {
 
-        $this->template->assign(array(
-            'menu' => $this->get_menu_v3(array(
-                fc_lang('升级PHP7CMS') => array('admin/upgrade/php7cms', 'refresh'),
-            )),
-        ));
-        $this->template->display('php7cms.html');
+	// 版本列表
+    public function vlist() {
+
+        $data = dr_catcher_data('http://www.poscms.net/version.php?cms=finecms');
+
+        if (!$data) {
+            exit('<p style="color:red;"> 暂时无法获取到服务器端版本信息 </p>');
+        }
+
+        if (strlen($data) > 20) {
+            exit('<p style="color:red;"> 返回数据不规范，请联系官方！ </p>');
+        }
+
+        $fwq = (string)(date('Y.m.d', strtotime($data)));
+        $azb = (string)(DR_UPDATE);
+        if ($fwq != $azb) {
+            $data.= '，'.'<span style="color:red">有新版本可更新</span>';
+
+        }
+
+
+        echo('<p> 本网站程序更新时间为： '.DR_UPDATE.'</a></p>');
+        exit('<p> <a href="https://gitee.com/dayrui/finecms/" style="color:green;" target="_blank">服务器程序更新时间为： '.$data.'</a></p>');
+
+
     }
-
 
 
 }
